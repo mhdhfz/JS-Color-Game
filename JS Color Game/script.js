642ae0581471977f70469ae33f4550e5c73e812c
Hafiz Mustafa <mhd.hfzz@gmail.com>
2019-01-14 12:00:35 +0800

    Add files via upload

--- JS Color Game/script.js
+++ JS Color Game/script.js
@@ -8,8 +8,9 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,9 +23,23 @@ for (var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
     if (clickedColor === pickedColor) {
-      alert("correct!");
+      messageDisplay.textContent = "Correct";
+      changeColors(clickedColor);
     } else {
-      alert("Wrong!!");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   });
+}
+
+function changeColors(color) {
+  // loop through all squares
+  for (var i = 0; i < colors.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
