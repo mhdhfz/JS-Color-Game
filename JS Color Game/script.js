1bd12c704ac01331a3b9a05b1bc7fdd707b6ff68
Hafiz Mustafa <mhd.hfzz@gmail.com>
2019-01-14 15:24:50 +0800

    Add files via upload

--- JS Color Game/script.js
+++ JS Color Game/script.js
@@ -1,16 +1,9 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-];
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,9 +15,11 @@ for (var i = 0; i < squares.length; i++) {
     //grab color of clicked square
     var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
+    console.log(clickedColor, pickedColor);
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct";
       changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     } else {
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again";
@@ -42,4 +37,27 @@ function changeColors(color) {
 function pickColor() {
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+function generateRandomColors(num) {
+  //make an array
+  var arr = [];
+  //add num random colors to array
+  for (var i = 0; i < num; i++) {
+    arr.push(randomColor());
+    // get random color and push into arr
+  }
+  //return that array
+  return arr;
+}
+
+function randomColor() {
+  // pick a "red" from 0 - 255
+  var r = Math.floor(Math.random() * 256);
+  // pick a "green" from 0 - 255
+  var g = Math.floor(Math.random() * 256);
+  // pick a "blue" from 0 - 255
+  var b = Math.floor(Math.random() * 256);
+  ("rgb(r, g, b)");
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
